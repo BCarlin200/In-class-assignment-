2d9ce873cf353f4decb41c1fe06edf06fa4b0a35
Brandon Carlin <angeloduck0123@gmail.com>
2020-01-09 11:27:42 -0700

    Completed in class activity

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+//Brandon Carlin
+//This is my own work
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//	double weight;
+//	cout << "Display your weight in pounds : " <<endl;
+//	cin >> weight;
+
+//	double kg = weight * 0.454;
+//		cout << "Your weight in kilograms is : " << static_cast<int>(weight * 0.454);
+
+
+//	double side;
+//	cout << "Display the side of the Hexagon : " << endl;
+//	cin >> side;
+
+//	double area = 5.2 / 2 * side * side;
+//	cout << "The area of the side of the Hexagon is : " << static_cast<int>(5.2 / 2 * side * side);
+
+
+//	int x, y;
+//	cout << "Enter first number : ";
+//	cin >> x;
+//	cout << "Enter second number : ";
+//		cin >> y;
+//		if (x > y)
+//		{
+//			cout << "First number " << x << " is the largest ";
+//		}
+//		else
+//		{
+//			cout << "Second number " << y << " is the largest ";
+//		}
+
+
+	return 0;
+}
